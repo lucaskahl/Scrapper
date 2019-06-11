18fb5bc3d746a468bd129cf6900088317081b364
lucaskahl <lucaschangkahl@gmail.com>
2019-06-10 21:42:51 -0300

    Recriado fluxo com puppeteer para pegar imagens

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,38 @@
-const fetch = require("node-fetch");
-const cheerio = require("cheerio");
+const puppeteer = require("puppeteer");
+const CREDS = require("./cred");
 
-const getData = async () => {
-  const url = "https://www.bible.com/pt";
+(async () => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
 
-  const response = await fetch(url);
-  const body = await response.text();
+    page.setUserAgent(
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    );
 
-  const $ = cheerio.load(body);
+    await page.goto("https://my.bible.com/pt/sign-in");
 
-  const data = [];
-  $("div.votd-content").each((i, item) => {
-    const $item = $(item);
-    const verse = $item
-      .find(".votd-verse")
-      .text()
-      .trim();
+    const EMAIL_SELECTOR = "input#signin-username";
+    const PASSWORD_SELECTOR = "input#signin-password";
+    const BUTTON_SELECTOR = "#signin_form button.solid-button.green.expand";
 
-    const dailyInformation = {
-      verse
-    };
+    await page.click(EMAIL_SELECTOR);
+    await page.keyboard.type(CREDS.username);
+    await page.click(PASSWORD_SELECTOR);
+    await page.keyboard.type(CREDS.password);
+    await page.click(BUTTON_SELECTOR);
 
-    data.push(dailyInformation);
-  });
-  console.log(data);
-};
+    await page.waitForSelector(".moment");
+    await page.click('.moment-bottom .md-menu a[title="Mais"]');
 
-getData();
+    setTimeout(async () => {
+      await page.click("a.md-button.md-ink-ripple");
+    }, 3500);
+
+    await page.waitForSelector(".yv-votd-image");
+
+    await page.waitForNavigation();
+  } catch (e) {
+    console.log("our error", e);
+  }
+})();
